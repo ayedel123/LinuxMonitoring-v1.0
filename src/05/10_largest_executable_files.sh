63ccb2711451cc656a6bd25694db1bafca9ff259
Willifer Feijoa <willifer@pr-d6.kzn.21-school.ru>
2024-07-03 16:23:51 +0300

    final?

--- src/05/10_largest_executable_files.sh
+++ src/05/10_largest_executable_files.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-exe_files=$(file -F" " $1* | grep " executable" | awk '{print $1}' )
-du -h $exe_files | sort -rh
-echo "$exe_files"
+exe_files=$(file -F" " $1* | grep " executable" | awk '{print $1}')
+md5="$(md5 -q $exe_files)"
+size=$(du -h $exe_files | sort -rh | awk '{print NR" - "$2", "$1", "}')
+
+
+echo "$size" > tmp_size
+echo "$md5" > tmp_md5
+
+echo "$(paste -d'\0' tmp_size tmp_md5)"
+
+rm tmp_size
+rm tmp_md5
