3c032eda6432cdbf14dbda444412828b31646804
willifer <willifer@student.21-school.ru>
2024-07-03 14:40:50 +0300

    02 fix

--- src/02/main.sh
+++ src/02/main.sh
@@ -2,11 +2,5 @@
 
 info=$(./get_info.sh)
 echo -e "$info"
-choice=''
 
-echo -e "\nWrite data in file? (y/n)"
-read choice
-
-if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
-    ./write_info.sh "$info"
-fi
+./write_info.sh "$info"
--- src/02/write_info.sh
+++ src/02/write_info.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+choice=''
+echo -e "\nWrite data in file? (y/n)"
+read choice
 
-status=$(date +"%d_%m_%y_%H_%M_%S").status 
-touch "$status"
-echo -e "$1" > ./"$status"
+if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
+    status=$(date +"%d_%m_%y_%H_%M_%S").status 
+    touch "$status"
+    echo -e "$1" > ./"$status"
+fi
